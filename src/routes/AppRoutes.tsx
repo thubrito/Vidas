8b815e1650425caa7aad8e64d3a4b885007e3e4f
thubrito <britoarthur150@gmail.com>
2025-12-03 22:49:50 -0300

    atualizando apppRoutes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,46 +1,20 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import App from "../App";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-import Cursos from "../pages/News";
 import Sobre from "../pages/About";
-import Login from "../pages/Login";
-import Termos from "../pages/Terms";
-import Privacidade from "../pages/Privacy";
 import FAQ from "../pages/faq";
-import Integrantes from "../pages/Members";
 import Contact from "../pages/Contact";
-import Chatbot from "../pages/NoraIA";
-import Dashboard from "../pages/Dashboard";
+import WithHeaderFooter from "../components/WithHeaderFooter";
 
 export default function AppRoutes() {
-  const isLoggedIn = localStorage.getItem("token");
-
   return (
     <Routes>
-      {/* Login sem layout */}
-      <Route path="/login" element={<Login />} />
-
-      {/* Termos e Privacidade sem layout App */}
-      <Route path="/termos" element={<Termos />} />
-      <Route path="/privacidade" element={<Privacidade />} />
-
-      {/* Rotas com layout App*/}
-      <Route
-        path="/"
-        element={isLoggedIn ? <App /> : <Navigate to="/login" replace />}
-      >
-        <Route index element={<Home />} />
-        <Route path="chatbot" element={<Chatbot />} />
-        <Route path="cursos" element={<Cursos />} />
-        <Route path="sobre" element={<Sobre />} />
-        <Route path="faq" element={<FAQ />} />
-        <Route path="integrantes" element={<Integrantes />} />
-        <Route path="contato" element={<Contact />} />
-        <Route path="dashboard" element={<Dashboard />} />
-      </Route>
+      <Route path="/" element={<WithHeaderFooter Component={Home} />} />
+      <Route path="/sobre" element={<WithHeaderFooter Component={Sobre} />} />
+      <Route path="/faq" element={<WithHeaderFooter Component={FAQ} />} />
+      <Route path="/contato" element={<WithHeaderFooter Component={Contact} />} />
 
       {/* Redirecionamento */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="*" element={<WithHeaderFooter Component={Home} />} />
     </Routes>
   );
 }
