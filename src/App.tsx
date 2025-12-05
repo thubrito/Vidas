d0cc8e3ca2c99647e1a8faf42e5da5bad80ef8d8
thubrito <britoarthur150@gmail.com>
2025-12-05 18:41:13 -0300

    removendo o componente ScrollToTop, que não é mais necessário para a navegação

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
-import ScrollToTop from "./components/ScrollToTop.tsx";
-
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
-      {/* ScrollToTop garante que a página vá para o topo ao navegar */}
-      <ScrollToTop />
 
       <main className="flex flex-col grow">
         <Outlet />
